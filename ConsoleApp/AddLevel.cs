e9d0593818262683e7e0f7099be9bd8e76612283
teklaos <bogdan.tyta@gmail.com>
2024-10-21 20:49:29 +0200

    Move AddLevel enumerator to Product class

--- ConsoleApp/AddLevel.cs
+++
@@ -1,7 +0,0 @@
-namespace ConsoleApp;
-
-public enum AddLevel {
-    Weak,
-    Medium,
-    Strong
-}
--- ConsoleApp/Product.cs
+++ ConsoleApp/Product.cs
@@ -1,5 +1,11 @@
 namespace ConsoleApp;
 
+public enum AddLevel {
+    Weak,
+    Medium,
+    Strong
+}
+
 public class Product {
     private string Name { get; set; } = null!;
     private int PricePerPound { get; set; }
