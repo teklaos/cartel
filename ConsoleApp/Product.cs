a593242e61432aa56a44d7202b2f5b0e8317e409
teklaos <bogdan.tyta@gmail.com>
2024-10-22 00:06:35 +0200

    Change PurityPercentage field to double

--- ConsoleApp/Product.cs
+++ ConsoleApp/Product.cs
@@ -9,6 +9,6 @@ public enum AddLevel {
 public class Product {
     private string Name { get; set; } = null!;
     private int PricePerPound { get; set; }
-    private int PurityPercentage { get; set; }
+    private double PurityPercentage { get; set; }
     private AddLevel AddictivenessLevel { get; set; }
 }
