0e8e86989f52be62b550812d6ee9fa342d4d7f9c
teklaos <bogdan.tyta@gmail.com>
2024-10-22 01:16:14 +0200

    Class extent example

--- ConsoleApp/Product.cs
+++ ConsoleApp/Product.cs
@@ -7,8 +7,17 @@ public enum AddLevelAttribute {
 }
 
 public class Product {
+    public static IEnumerable<Product> Products { get; private set; } = []; // Class extent
     public string Name { get; set; } = null!;
     public int PricePerPound { get; set; }
     public double PurityPercentage { get; set; }
     public AddLevelAttribute AddictivenessLevel { get; set; }
+
+    public Product(string name, int pricePerPound, double purityPercentage, AddLevelAttribute addictivenessLevel) {
+        this.Name = name;
+        this.PricePerPound = pricePerPound;
+        this.PurityPercentage = purityPercentage;
+        this.AddictivenessLevel = addictivenessLevel;
+        Products = Products.Append(this);
+    }
 }
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -2,6 +2,10 @@
 
 public class Program {
     public static void Main(string[] args) {
-        Console.WriteLine("Hi!");
+        Product crystalMethamphetamine = new Product("Crystal Methamphetamine", 2500, 98.71, AddLevelAttribute.Strong);
+        Product cocaine = new Product("Cocaine", 3000, 95.62, AddLevelAttribute.Medium);
+        foreach (Product prod in Product.Products) {
+            Console.WriteLine(prod.Name);
+        }
     }
 }
