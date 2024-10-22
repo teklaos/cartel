8f686579aeebb22008d6c06d9eb1e7715e76b0fc
teklaos <bogdan.tyta@gmail.com>
2024-10-22 20:53:46 +0200

    Rename Deal to Distributor_Customer

--- ConsoleApp/Distributor_Customer.cs
+++ ConsoleApp/Distributor_Customer.cs
@@ -1,11 +1,11 @@
 namespace ConsoleApp;
 
-public class Deal {
+public class Distributor_Customer {
     public DateTime DealStartDate { get; set; }
     public int AmountOfProduct { get; set; }
     public DateTime? DealEndDate { get; set; }
 
-    public Deal(DateTime dealStartDate, int amountOfProduct, DateTime? dealEndDate) {
+    public Distributor_Customer(DateTime dealStartDate, int amountOfProduct, DateTime? dealEndDate) {
         this.DealStartDate = dealStartDate;
         this.AmountOfProduct = amountOfProduct;
         this.DealEndDate = dealEndDate;
