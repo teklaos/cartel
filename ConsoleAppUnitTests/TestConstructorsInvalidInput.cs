5add8fd9a6c79b5efc2c5220a23af6846a4952a7
teklaos <bogdan.tyta@gmail.com>
2024-11-07 01:33:39 +0100

    Minor styling fixes

--- ConsoleAppUnitTests/TestConstructorsInvalidInput.cs
+++ ConsoleAppUnitTests/TestConstructorsInvalidInput.cs
@@ -2,7 +2,7 @@ using ConsoleApp.models;
 
 namespace ConsoleAppUnitTests;
 
-public class TestsBusinessLogic {
+public class TestConstructorsInvalidInput {
     [Test]
     public void ConstructorValidInput() {
         string name = "Walter White";
@@ -18,18 +18,7 @@ public class TestsBusinessLogic {
             Assert.That(chemist.RulesToFollow, Is.EqualTo(rules));
             Assert.That(chemist.PoundsCooked, Is.EqualTo(poundsCooked));
         });
-        Assert.Contains(chemist, Chemist._chemists.ToList());
-    }
-
-    [Test]
-    public void ConstructorInvalidPoundsCookedValue() {
-        string name = "Jesse Pinkman";
-        int trustLevel = 7;
-        var rules = new List<string> {"Loyalty to the crew.", "Avoid police attention."};
-        int poundsCooked = -10;
-
-        Assert.Throws<ArgumentException>(() => new Chemist(name, trustLevel, rules, poundsCooked),
-            "Expected ArgumentException for negative cooked pounds.");
+        Assert.That(Chemist._chemists.ToList(), Does.Contain(chemist));
     }
 
     [Test]
@@ -44,6 +33,17 @@ public class TestsBusinessLogic {
     }
 
     [Test]
+    public void ConstructorInvalidTrustLevel() {
+        string name = "Jesse Pinkman";
+        int trustLevel = -1;
+        var rules = new List<string> {"Loyalty to the crew.", "Avoid police attention."};
+        int poundsCooked = 150;
+
+        Assert.Throws<ArgumentException>(() => new Chemist(name, trustLevel, rules, poundsCooked),
+            "Expected ArgumentException for negative trust level.");
+    }
+
+    [Test]
     public void ConstructorInvalidRules() {
         string name = "Gale Boetticher";
         int trustLevel = 9;
@@ -52,5 +52,16 @@ public class TestsBusinessLogic {
 
         Assert.Throws<ArgumentException>(() => new Chemist(name, trustLevel, rules, poundsCooked),
             "Expected ArgumentException for null rules to follow collection.");
+    }
+    
+    [Test]
+    public void ConstructorInvalidPoundsCookedValue() {
+        string name = "Krazy-8";
+        int trustLevel = 2;
+        var rules = new List<string> {"Do not die.", "Do not kill Walter White with a glass shard."};
+        int poundsCooked = -1;
+
+        Assert.Throws<ArgumentException>(() => new Chemist(name, trustLevel, rules, poundsCooked),
+            "Expected ArgumentException for negative cooked pounds.");
     }
 }
