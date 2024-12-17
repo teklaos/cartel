232dbd6ee121598664066d5a6bf07ecbe50a2396
teklaos <bogdan.tyta@gmail.com>
2024-12-17 19:01:53 +0100

    refactor: Minor styling fixes

--- ConsoleAppUnitTests/TestAssociations.cs
+++ ConsoleAppUnitTests/TestAssociations.cs
@@ -8,11 +8,11 @@ public class TestAssociations {
         Warehouse warehouse = new("Warsaw, Praga", 1000);
         Product product = new("Meth", 1000, AddLevelAttribute.Strong);
 
-        warehouse.AddProductToWarehouse(product);
+        warehouse.AddProduct(product);
 
         Assert.Multiple(() => {
             Assert.That(Warehouse.AssociatedProducts, Has.Count.EqualTo(Product.Products.Count));
-            Assert.That(Warehouse.Warehouses, Has.Count.EqualTo(Product.ConnectedWarehouses.Count));
+            Assert.That(Warehouse.Warehouses, Has.Count.EqualTo(Product.AssociatedWarehouses.Count));
         });
     }
 
@@ -21,11 +21,11 @@ public class TestAssociations {
         Warehouse warehouse = new("Warsaw, Praga", 1000);
         Product product = new("Meth", 1000, AddLevelAttribute.Strong);
 
-        product.AddProductStoredIn(warehouse);
+        product.AddWarehouse(warehouse);
 
         Assert.Multiple(() => {
             Assert.That(Warehouse.AssociatedProducts, Has.Count.EqualTo(Product.Products.Count));
-            Assert.That(Warehouse.Warehouses, Has.Count.EqualTo(Product.ConnectedWarehouses.Count));
+            Assert.That(Warehouse.Warehouses, Has.Count.EqualTo(Product.AssociatedWarehouses.Count));
         });
         // Console.WriteLine($"Warehouses connected = {Warehouse.AssociatedProducts.Count}");
         // Console.WriteLine($"Products connected = {Warehouse.AssociatedProducts.Count}");
@@ -36,10 +36,10 @@ public class TestAssociations {
         Warehouse warehouse = new("Warsaw, Praga", 1000);
         Product product = new("Meth", 1000, AddLevelAttribute.Strong);
 
-        product.AddProductStoredIn(warehouse);
-        product.RemoveProductStoredIn(warehouse);
+        product.AddWarehouse(warehouse);
+        product.RemoveWarehouse(warehouse);
 
-        Assert.That(Warehouse.AssociatedProducts, Has.Count.EqualTo(Product.ConnectedWarehouses.Count));
+        Assert.That(Warehouse.AssociatedProducts, Has.Count.EqualTo(Product.AssociatedWarehouses.Count));
     }
 
     [Test]
@@ -47,9 +47,9 @@ public class TestAssociations {
         Warehouse warehouse = new("Warsaw, Praga", 1000);
         Product product = new("Meth", 1000, AddLevelAttribute.Strong);
 
-        product.AddProductStoredIn(warehouse);
-        warehouse.RemoveProductFromWarehouse(product);
+        product.AddWarehouse(warehouse);
+        warehouse.RemoveProduct(product);
 
-        Assert.That(Warehouse.AssociatedProducts, Has.Count.EqualTo(Product.ConnectedWarehouses.Count));
+        Assert.That(Warehouse.AssociatedProducts, Has.Count.EqualTo(Product.AssociatedWarehouses.Count));
     }
 }
